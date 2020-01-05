8a26ad111f0b413f0c48f839667dddb568c02b4e
Ushinji <siguma971@gmail.com>
2020-01-05 14:13:01 +0900

    Add the procesedure of getting font address.

--- src/17_draq_plane/kernel.s
+++ src/17_draq_plane/kernel.s
@@ -6,6 +6,14 @@
 [BITS 32]
 ; エントリポイント
 kernel:
+    ; フォントアドレスの取得
+    mov esi, BOOT_LOAD + SECT_SIZE
+    movzx eax, word [esi + 0]
+    movzx ebx, word [esi + 2]
+    shl eax, 4
+    add eax, ebx
+    mov [FONT_ADR], eax
+
     ; 処理終了
     jmp $
 
