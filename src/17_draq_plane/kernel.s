d7786a2f5e5a31a1cef743dd66ff2680bc3ff943
Ushinji <siguma971@gmail.com>
2020-01-23 21:00:43 +0900

    Add display of water blue dot.

--- src/17_draq_plane/kernel.s
+++ src/17_draq_plane/kernel.s
@@ -14,6 +14,22 @@ kernel:
     add eax, ebx
     mov [FONT_ADR], eax
 
+    ; オフセット計算
+    shl edi, 8  ; EDI *= 256
+    lea edi, [edi * 4 + edi + 0xA_0000] ; EDI = 0xA_0000[EDI * 4 + EDI]; // VRAMアドレス
+
+    ; 水色で表示する場合
+    mov ah, 0x04    ; AH = 書き込みプレーンを指定
+    mov al, 0x02    ; AL = マップマスクレジスタ(書き込みプレーンを指定)
+    mov dx, 0x03C4  ; 0x03C4
+    out dx, ax      ; // ポート出力(プレーンの選択)
+
+    mov [0x00A_0000], byte 0x00
+    mov ah, 0x0B    ; 書き込みプレーンを指定
+    out dx, ax      ; ポート出力(プレーン選択)
+
+    mov [0x000A_0000], byte 0x80 ; ビットパターンの書き込み
+    
     ; 処理終了
     jmp $
 
